1324dcbfdd8bab364a565bea64e3cf37bf474139
marioroz19-netizen <marioroz19@gmail.com>
2026-08-25 10:48:27 +0200

    Update CTASection.tsx

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -3,7 +3,7 @@ import Reveal from "./Reveal";
 
 export default function CTASection() {
   return (
-    <Reveal className="pt-16 pb-8 text-center">
+    <Reveal className="pt-24 pb-12 text-center">
       <Link
         href="/contacto"
         className="inline-block bg-accent text-bg font-bold text-[14px] px-8 py-4 rounded-xl hover:opacity-90 transition-opacity"
